c733d94ba070bbe6961493c8d63f039c8ccb7e43
Oscar94 <oscar310394@gmail.com>
2017-09-14 13:45:55 -0600

    Example of Dynamic  PHP

---
+++ WorkShop1/dynamic.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Workshop Dynamic</title>
+    <?php 
+    $txt="Hola Mundo";
+    ?>
+</head>
+
+<body>
+       <div>
+            <?php
+                echo $txt;
+            ?>
+            <iframe src="https://giphy.com/embed/pE1hiLxlgVz8I" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/pE1hiLxlgVz8I"></a></p>
+        </div>
+</body>
+
+</html>
